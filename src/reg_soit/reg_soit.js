6e3ec5936664653317b06d9da4b5fe4dac0f6882
kelluts <kelluts@gmail.com>
2017-03-25 01:24:59 +0200

    backendiga ühendus, cors, formist andmebaasi saatmine

--- src/reg_soit/reg_soit.js
+++ src/reg_soit/reg_soit.js
@@ -1,9 +1,39 @@
-import {HttpClient} from 'aurelia-fetch-client'
+import {HttpClient, json} from 'aurelia-fetch-client'
 import environment from '../environment'
 
 export class reg_soit {
-    addRide() {
-        console.log("Method executed!")
+   
+    
+   var userData = {};
+    
+
+	
+ 
+    activate() {
+        let client = new HttpClient();
+        
+        
         client.fetch(environment.serverURL + 'rides/all')
+        .then(response => response.json())
+        .then(users => this.userList = users);
     }
+    
+    
+    
+    addRide() {
+		let client = new HttpClient();
+        var userData = {}
+        
+
+		client.fetch(environment.serverURL + 'rides/create', {
+			'method': "POST",
+			'body': json(this.userData)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Server saatissad " + data.lahteKoht);
+            console.log(userData);
+		});
+        	console.log("Method executed!")
+	}
 }
