6bbd3126f9efad477e01afb1fe0d92ef7319ac18
sirelint <sirelin.tallo@gmail.com>
2017-03-27 17:12:56 +0300

    top-nav bar

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 export class App {
-  constructor() {
-    this.message = 'hei hei!';
-  }
+  
     configureRouter(config, router) {
     this.router = router;
     config.title = 'My Aurelia app';
     config.map([
-      { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
-      { route: 'choice',            name: 'choice',      moduleId: 'choice/front-page',   nav: true },
-      { route: 'vedaja',            name: 'vedaja',      moduleId: 'vedaja/vedaja',   nav: true },
-      { route: 'saatja',            name: 'saatja',      moduleId: 'saatja/saatja',   nav: true },
-      { route: 'reg_soit',            name: 'reg_soit',      moduleId: 'reg_soit/reg_soit',   nav: true },
+      { route: ['', 'home'],       name: 'home',       moduleId: 'home/index', title:"Home", nav: true },
+      { route: 'choice',            name: 'choice',      moduleId: 'choice/front-page', title:"Valik",   nav: true },
+      { route: 'vedaja',            name: 'vedaja',      moduleId: 'vedaja/vedaja', title:"Vedaja",   nav: true },
+      { route: 'saatja',            name: 'saatja',      moduleId: 'saatja/saatja', title:"Saatja",   nav: true },
+      { route: 'reg_soit',            name: 'reg_soit',      moduleId: 'reg_soit/reg_soit', title:"Registreeri sõit",   nav: true },
       
       
     ]);
