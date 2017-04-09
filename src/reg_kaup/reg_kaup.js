3979c4fc53f5d36f931eda9709cf317490ea505b
sirelint <sirelin.tallo@gmail.com>
2017-04-09 14:33:49 +0300

    registreeri kaup

---
+++ src/reg_kaup/reg_kaup.js
@@ -0,0 +1,29 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+import environment from '../environment'
+
+var userData={};
+var userList=[];
+
+export class reg_soit {
+    contructor(userData){
+        this.userData = userData;
+    }
+    
+    
+    addRide() {
+		let client = new HttpClient();
+        
+        
+
+		client.fetch(environment.serverURL + 'rides/create', {
+			'method': "POST",
+			'body': json(this.userData)
+		})
+			.then(response => response.json())
+			.then(userData => {
+				console.log("Server saatis ära, networkist create pealt naeb, andmebaasis peaks ka olema");
+            console.log(userData);
+		});
+        	console.log("Method executed!")
+	}
+};
