0748d29c766cc3c0448bb9a2b6f0443d44fb7303
sirelint <sirelin.tallo@gmail.com>
2017-05-17 13:09:45 +0300

    Revert "Revert "Merge branch 'auth'""
    This reverts commit 02f5e740919f0490ebc3f1f663e8f54e2f519988.

---
+++ Aurelia/src/app.js
@@ -0,0 +1,27 @@
+import {inject} from 'aurelia-framework';
+import {FetchConfig} from 'aurelia-auth';
+
+@inject(FetchConfig)
+export class App {
+    constructor(fetchConfig) {
+	this.fetchConfig = fetchConfig;
+}
+
+configureRouter(config, router) {
+    this.router = router;
+    this.fetchConfig.configure();
+    config.title = 'My Aurelia App';
+
+    config.map([
+      { route: ['', 'home'],       name: 'home',       moduleId: 'home/index', title:"Home", nav: true },
+      { route: 'logout',             name:'logout',      moduleId: 'logout/logout',     nav: false, title:'Logout' },
+      { route: 'choice',            name: 'choice',      moduleId: 'choice/front-page', title:"Valik",   nav: true },
+      { route: 'vedaja',            name: 'vedaja',      moduleId: 'vedaja/vedaja', title:"Vedaja",   nav: true, auth:true },
+      { route: 'saatja',            name: 'saatja',      moduleId: 'saatja/saatja', title:"Saatja",   nav: true },
+      { route: 'reg_soit',            name: 'reg_soit',      moduleId: 'reg_soit/reg_soit', title:"Registreeri sõit",   nav: true },
+      { route: 'reg_kaup',            name: 'reg_kaup',      moduleId: 'reg_kaup/reg_kaup', title:"Registreeri vedu",   nav: true },
+      
+      
+    ]);
+  }
+}
