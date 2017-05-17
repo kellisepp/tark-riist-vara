73487dff480747cd13cb250680a1e1fec8e2d86a
sirelint <sirelin.tallo@gmail.com>
2017-05-17 11:27:20 +0300

    Revert "poolik google erroriga"
    This reverts commit 8e477e395ea9c4c70b4bd359d34a851cd5b2f090.

--- VedajaAS/src/main/java/romaniancoder/booking/BookingDemoApplication.java
+++ VedajaAS/src/main/java/romaniancoder/booking/BookingDemoApplication.java
@@ -11,8 +11,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+<<<<<<< HEAD
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+=======
+>>>>>>> parent of 8e477e3... poolik google erroriga
 
 @Configuration
 @ComponentScan
